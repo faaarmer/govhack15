5c13423d39948169c8be394d6afc95f0f6a1a8f6
faaarmer <faaarmer@gmail.com>
2015-07-04 15:48:48 +1000

    Changed the base map layer.

--- assets/js/map_test.js
+++ assets/js/map_test.js
@@ -20,9 +20,17 @@ var Map_test = {
 		var layerUrl = 'https://faaarmer.cartodb.com/api/v2/viz/31908478-2206-11e5-882a-0e8dde98a187/viz.json';
 
 		// Add a basemap to the map object just created
-	    L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
-	        attribution: 'Stamen'
-	    }).addTo(Map_test.map);
+
+		var HERE_normalNight = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.night/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+			attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
+			subdomains: '1234',
+			mapID: 'newest',
+			app_id: 'KuYppsdXZznpffJsKT24',
+			app_code: 'A7tBPacePg9Mj_zghvKt9Q',
+			base: 'base',
+			maxZoom: 20
+		});
+	    HERE_normalNight.addTo(Map_test.map);
 
 		cartodb.createLayer(Map_test.map, layerUrl)
 	    .addTo(Map_test.map).on('done', function(layer) {
