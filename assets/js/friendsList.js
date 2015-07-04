622ce3ac819bd67952e35239816a03e32a8cd333
Hans Einner Barroga <hanseinnerbarroga@d-a140-227.external.eduroam.uq.edu.au>
2015-07-05 03:11:34 +1000

    Show check status on button, not "Waiting"

--- assets/js/friendsList.js
+++ assets/js/friendsList.js
@@ -95,7 +95,7 @@ var FriendsList = {
 		$.each(friends, function(index, friend){
 			var friend_id = friend.rId;
 			var friendName = friend.fullName;
-			var friendsHtml = '<div class="row friend-list-item"><div class="col-xs-6" data-rid="' + friend_id + '"><h6 class="friend-name">' + friendName + '</h6></div><div class="col-xs-6 right-status"><span class="safety-status"><button id="safetyButton" class="btn btn-xs hide">Check safety</button><button id="awaitingResponse" class="btn btn-xs ">Waiting...</button><button id="okStatus" class="btn btn-xs hide"><span class="glyphicon glyphicon-ok"></span> I\'m Safe</button><button id="helpStatus" class="btn btn-xs hide"> I need help!</button></span><button id="deleteButton" class="btn btn-xs"><span class="glyphicon glyphicon-remove"></span></button></div></div>';
+			var friendsHtml = '<div class="row friend-list-item"><div class="col-xs-6" data-rid="' + friend_id + '"><h6 class="friend-name">' + friendName + '</h6></div><div class="col-xs-6 right-status"><span class="safety-status"><button id="safetyButton" class="btn btn-xs">Check safety</button><button id="awaitingResponse" class="btn btn-xs hide">Waiting...</button><button id="okStatus" class="btn btn-xs hide"><span class="glyphicon glyphicon-ok"></span> I\'m Safe</button><button id="helpStatus" class="btn btn-xs hide"> I need help!</button></span><button id="deleteButton" class="btn btn-xs"><span class="glyphicon glyphicon-remove"></span></button></div></div>';
 			//'<li class="friends-list" data-fid="' + friend_id + '">' + friendName + '</li>';
 			$('#friends').append(friendsHtml);
 		});
