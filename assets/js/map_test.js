6c4c8aa4ede6ba54f07879bb8097163e2d91825a
Daniel Farmer <faaarmer@gmail.com>
2015-07-04 19:19:05 +1000

    Added toggling, hid the data we don't have and added firestation data

--- assets/js/map_test.js
+++ assets/js/map_test.js
@@ -1,17 +1,22 @@
 var Map_test = {
 
 	map: null,
+	bushFiresLayer: '',
+	fireStationsLayer: '',
 
 	init: function(){
 		Map_test.map_init();
+		Map_test.events();
 	},
 
 	events: function(){
+		$('body').on('click','.bushfires_toggle',function(e) {
+			Map_test.bushFiresLayer.toggle();
+		});
 
-	},
-
-	pos: function() {
-		return control.getPosition();
+		$('body').on('click','.emergency_toggle',function(e) {
+			Map_test.fireStationsLayer.toggle();
+		});
 	},
 
 	map_init: function(){
@@ -25,14 +30,16 @@ var Map_test = {
 		 var zoomControl = L.control.zoom({
             position: 'topright'
         });
-		 
+
         Map_test.map.addControl(zoomControl);
 
-		var layerUrl = 'https://faaarmer.cartodb.com/api/v2/viz/31908478-2206-11e5-882a-0e8dde98a187/viz.json';
+		var bushFiresURL = 'https://faaarmer.cartodb.com/api/v2/viz/31908478-2206-11e5-882a-0e8dde98a187/viz.json';
+		var fireStationsURL = 'https://faaarmer.cartodb.com/api/v2/viz/790bff40-2228-11e5-89f4-0e4fddd5de28/viz.json';
+
 
 		// Add a basemap to the map object just created
 
-		var HERE_normalNight = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.night/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
+		var baseMap = L.tileLayer('http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/maptile/{mapID}/normal.night/{z}/{x}/{y}/256/png8?app_id={app_id}&app_code={app_code}', {
 			attribution: 'Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>',
 			subdomains: '1234',
 			mapID: 'newest',
@@ -41,13 +48,20 @@ var Map_test = {
 			base: 'base',
 			maxZoom: 20
 		});
-	    HERE_normalNight.addTo(Map_test.map);
 
-		cartodb.createLayer(Map_test.map, layerUrl)
-	    .addTo(Map_test.map).on('done', function(layer) {
+	    baseMap.addTo(Map_test.map);
 
-	    }).on('error', function() {
-	      //log the error
+	    cartodb.createLayer(Map_test.map, bushFiresURL).addTo(Map_test.map).on('done', function(layer) {
+	    	Map_test.bushFiresLayer = layer;
+	    }).on('error', function(error) {
+	      console.log(error);
+	    });
+
+	    cartodb.createLayer(Map_test.map, fireStationsURL).addTo(Map_test.map).on('done', function(layer) {
+	    	Map_test.fireStationsLayer = layer;
+	    	Map_test.fireStationsLayer.hide();
+	    }).on('error', function(error) {
+	      console.log(error);
 	    });
 	}
 };
