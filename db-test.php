4ac6503e93bb5dce7bc7225832d4d9de34b47eaa
faaarmer <faaarmer@gmail.com>
2015-07-04 14:31:06 +1000

    db connection test

--- db-test.php
+++ db-test.php
@@ -1,3 +1,8 @@
 <?php
 
-$mysqli = new mysqli('govhack2915db.database.windows.net','Adam','Amoamo123','govhackdb');
+$mysqli = new mysqli('au-cdbr-azure-southeast-a.cloudapp.net','bfeace5cc210be','cfe2e899','govhack2015bne');
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
