4797d6e1a28481bc2ead4d14739f96713b021be4
Mark183 <mwganser@gmail.com>
2015-07-05 12:22:47 +1000

    change status stuff

--- assets/js/friendsList.js
+++ assets/js/friendsList.js
@@ -102,11 +102,10 @@ var FriendsList = {
 			var friend_id = friend.rId;
 			var friendName = friend.fullName;
 			var friendsHtml = '<div class="row friend-list-item"><div class="cool col-xs-6" data-status="' + dataStat +'" data-rid="' + friend_id + '"><h6 class="friend-name">' + friendName + '</h6></div><div class="col-xs-6 right-status"><span class="safety-status"><button id="safetyButton" class="btn btn-xs">Check safety</button><button id="awaitingResponse" class="btn btn-xs hide">Waiting...</button><button id="okStatus" class="btn btn-xs hide"><span class="glyphicon glyphicon-ok"></span> I\'m Safe</button><button id="helpStatus" class="btn btn-xs hide"> I need help!</button></span><button id="deleteButton" class="btn btn-xs"><span class="glyphicon glyphicon-remove"></span></button></div></div>';
-			//'<li class="friends-list" data-fid="' + friend_id + '">' + friendName + '</li>';
 			$('#friends').append(friendsHtml);
 
 		});
-		console.log($('[data-status="1"'));
+
 		$('[data-status="1"').closest('.friend-list-item').find('#safetyButton').addClass('hide');
 		$('[data-status="1"').closest('.friend-list-item').find('#okStatus').removeClass('hide');
 
@@ -119,7 +118,20 @@ var FriendsList = {
 
 	sendMessage: function(){
 		var $this = $(this);
+		console.log($this);
+		var status = $this.closest('.friend-list-item').find('.cool').attr('data-status');
+		if(status != 'null'){
+			console.log("no send");
+			console.log(status);
+			return;
+		}
+
+		$this.addClass('hide');
+		$this.closest('.friend-list-item').find('#awaitingResponse').removeClass('hide');
+		$this.closest('.friend-list-item').find('.cool').attr('data-status', 3);
+
 		var rId = $this.closest('.friend-list-item').find('.cool').attr('data-rid');
+
 		if($this.attr('id') == 'safetyButton'){
 			$.ajax({
 				url: 'assets/php/createSMS.php',
@@ -134,8 +146,8 @@ var FriendsList = {
 					if(!result.success){
 						console.log(result);
 					}else{
-						$this.addClass('hide');
-						$this.closest('.friend-list-item').find('#awaitingResponse').removeClass('hide');
+						console.log(result);
+						
 					}
 
 				}
