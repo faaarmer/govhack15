3c3d9b45811481e9e02e71e752592cd0de80ac8b
Daniel Farmer <faaarmer@gmail.com>
2015-07-05 11:54:37 +1000

    test

--- assets/php/status.php
+++ assets/php/status.php
@@ -1,7 +1,9 @@
 <?php 
 
 include_once 'connection.php';
+require('../../vendor/autoload.php');
 
+$pusher = new Pusher('9b198123b867650ff8cb', 'cc8bbf10251c6efbd0d2', '128293');
 
 $response = array();
 /* check connection */
@@ -19,6 +21,16 @@ $date = date("'Y-m-d H:i:s'");
 $sql = "INSERT INTO status (rid, sCode, lat, lon, sTimestamp) VALUES ($rid,$status,$lat,$lon,$date) ON DUPLICATE KEY UPDATE sCode = $status, lat=$lat,lon=$lon,sTimestamp=$date;";
 if (mysqli_query($mysqli, $sql)) {
 	   $response['status'] = "success";
+	   $data = array();
+	   $result = $mysqli->query("SELECT uId FROM users, relations WHERE relations.rid = '$rid' AND relations.uId = users.uId");
+	   $row = $result->fetch_assoc();
+	   echo $row['uId'];
+	   $data['rid'] = $rid;
+	   $data['status'] = $status;
+	   $data['lat'] = $lat;
+	   $data['lon'] = $lon;
+	   $data['date'] = $date;
+	   $pusher->trigger($row['uId'], 'my_event', $data);
 } else {
 		error_log(mysqli_error($mysqli));
 	   $response['status'] = "failed";
